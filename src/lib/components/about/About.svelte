6dc4e436ebf6dc1edb7d7f2132bf36e2cc97e54e
Paddy <patrick@firefinch.io>
2022-01-04 17:20:06 +0000

    added sections and margins

--- src/lib/components/about/About.svelte
+++ src/lib/components/about/About.svelte
@@ -1,2 +1,20 @@
-<h2>About</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus urna duis convallis convallis. Magna ac placerat vestibulum lectus. Eros in cursus turpis massa tincidunt. Pharetra sit amet aliquam id diam maecenas. Praesent semper feugiat nibh sed pulvinar. Commodo ullamcorper a lacus vestibulum sed arcu non. Diam quam nulla porttitor massa id neque aliquam vestibulum morbi. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Viverra nam libero justo laoreet. Id diam vel quam elementum pulvinar etiam non quam lacus. Arcu non sodales neque sodales ut etiam sit amet nisl. Dictumst quisque sagittis purus sit amet volutpat. Quis ipsum suspendisse ultrices gravida dictum fusce ut. Etiam erat velit scelerisque in.</p>
+<section id="about">
+	<h2>About</h2>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua. Lectus urna duis convallis convallis. Magna ac placerat
+		vestibulum lectus. Eros in cursus turpis massa tincidunt. Pharetra sit amet aliquam id diam
+		maecenas. Praesent semper feugiat nibh sed pulvinar. Commodo ullamcorper a lacus vestibulum sed
+		arcu non. Diam quam nulla porttitor massa id neque aliquam vestibulum morbi. Consectetur
+		adipiscing elit duis tristique sollicitudin nibh sit amet. Viverra nam libero justo laoreet. Id
+		diam vel quam elementum pulvinar etiam non quam lacus. Arcu non sodales neque sodales ut etiam
+		sit amet nisl. Dictumst quisque sagittis purus sit amet volutpat. Quis ipsum suspendisse
+		ultrices gravida dictum fusce ut. Etiam erat velit scelerisque in.
+	</p>
+</section>
+
+<style>
+	section {
+		margin: 8px;
+	}
+</style>
--- src/lib/components/services/Services.svelte
+++ src/lib/components/services/Services.svelte
@@ -1,6 +1,12 @@
-<h2>Services</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<section id="services">
+    <h2>Services</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</section>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<style>
+    section{
+        margin: 8px;
+    }
+</style>
