6cfa0fb5bf6e1c226e1ce69a3e3b139374f39f2d
Paddy <patrick@firefinch.io>
2022-01-23 13:57:17 +0000

    more about

--- src/lib/components/about/About.svelte
+++ src/lib/components/about/About.svelte
@@ -1,23 +1,20 @@
 <section id="about">
 	<h2>About</h2>
 	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-		labore et dolore magna aliqua. Lectus urna duis convallis convallis. Magna ac placerat
-		vestibulum lectus. Eros in cursus turpis massa tincidunt. Pharetra sit amet aliquam id diam
-		maecenas. Praesent semper feugiat nibh sed pulvinar. Commodo ullamcorper a lacus vestibulum sed
-		arcu non. Diam quam nulla porttitor massa id neque aliquam vestibulum morbi. Consectetur
-		adipiscing elit duis tristique sollicitudin nibh sit amet. Viverra nam libero justo laoreet. Id
-		diam vel quam elementum pulvinar etiam non quam lacus. Arcu non sodales neque sodales ut etiam
-		sit amet nisl. Dictumst quisque sagittis purus sit amet volutpat. Quis ipsum suspendisse
-		ultrices gravida dictum fusce ut. Etiam erat velit scelerisque in.
+		Before I was a software developer I ran a small business and wanted a website to increase my customer base by 
+		being easy to find and letting people know what I offered. I was overwhelmed by the various options and also 
+		the high recurring costs.
 	</p>
 	<p>
-		Before I was a software developer I ran a small business and wanted a website to increase my customer base and 
-		tell my story. I was overwhelmed by the various options and also the high recurring costs.
+		I want to help small businesses who where in the position I was and help them get a cheap to run, fast, SEO 
+		friendly website with a professional email address. I spent far too much time trying to do this myself when I 
+		could have been doing better things for my business and in the end I was also paying far more than I needed to
+		for what I needed.
 	</p>
 	<p>
-		I want to help small businesses who where in the position I was and help them get a cheap to run, fast and SEO 
-		friendly website and a professional email address.
+		Through my experience as a software developer I have tried and tested different technologies and ways to host them
+		online and I can get your website hosted and email set up and the only recurring cost for you is to pay for your domain 
+		(you can't get round this one).
 	</p>
 </section>
 
